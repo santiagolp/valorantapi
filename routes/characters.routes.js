f4a4de6c4e7cb6aa04acae11916181b1e3b45b24
Santiago Dellepiane <dellepiane.sj@gmail.com>
2024-08-09 12:08:04 -0300

    feat: Add API endpoint character/:id to api-docs

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -77,6 +77,46 @@ router.get('/', character.getCharacters);
  */
 
 router.get('/all', character.getCharactersWithSkils);
+
+/**  
+* @swagger
+* /characters/{id}:
+*  get:
+*    summary: Obtiene una lista de habilidades por id de personaje
+*    description: Devuelve una lista de habilidades por id de personaje.
+*    parameters:
+*      - in: path
+*        name: id
+*        required: true
+*        description: Id del personaje
+*        schema:
+*          type: integer
+*    responses:
+*      200:
+*        description: Lista de habilidades devuelta exitosamente
+*        content:
+*          application/json:
+*            schema:
+*              type: array
+*              items:
+*                type: object
+*                properties:
+*                  id:
+*                    type: integer
+*                    example: 1
+*                  name:
+*                    type: string
+*                    example: Toxic Screen
+*                  description:
+*                    type: string
+*                    example: Deploy a long line of gas emitters that you can reactivate to create a wall of toxic gas.
+*                  characterId:
+*                    type: integer
+*                    example: 1
+*    tags:
+*      - Person
+*      - Habilidades
+*/
 router.get('/:id', character.getSkillsByCharacterId);
 
 
